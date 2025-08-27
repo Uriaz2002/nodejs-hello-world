be352afe1a0004db0ee213a37dadcf4bed3b2472
Uriaz2002 <84433286+Uriaz2002@users.noreply.github.com>
2025-08-27 12:11:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
-  res.end('Hello World');
+  res.end('Hello World from OpenShift!');
 });
-server.listen(8080);
+
+const PORT = process.env.PORT || 8080;
+const HOST = '0.0.0.0';
+
+server.listen(PORT, HOST, () => {
+  console.log(`Server running at http://${HOST}:${PORT}/`);
+});
